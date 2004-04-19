2f8eca02de8eedec53867361d505439b361b3a64
HD Moore <hd_moore@rapid7.com>
2004-04-19 03:10:02 +0000

    Fixed crash bug with FindSocket... this was in release :(

--- lib/Msf/PayloadComponent/FindConnection.pm
+++ lib/Msf/PayloadComponent/FindConnection.pm
@@ -17,18 +17,19 @@ sub ChildHandler {
 
   my $selector = IO::Select->new($sock);
 
-  my @ready = $selector->can_write(.5);
+  my @ready = $selector->can_write(0.5);
 
-  goto DONE if(!@ready);
+  goto DONE if(!@ready || !$ready[0]->connected);
 
-  $ready[0]->send("echo ABCDE \r\n");
+  $ready[0]->send("echo ABCDE\r\n");
 
-  @ready = $selector->can_read(.5);
+  @ready = $selector->can_read(0.5);
 
-  goto DONE if(!@ready);
+  goto DONE if(!@ready || !$ready[0]->connected);
 
   my $data;
   $ready[0]->recv($data, 4096);
+  
   goto DONE if(!length($data));
   if($data =~ /ABCDE/) {
     $self->SocketIn($ready[0]);
@@ -45,6 +46,5 @@ DONE:
 
 sub SigHandler {
   my $self = shift;
-  # shhh
 }
 1;
