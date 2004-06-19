77fd184890f32f04056160a0c2e5538a1252e69d
Spoon M <spoonm@gmail.com>
2004-06-19 08:41:29 +0000

    arg

--- encoders/ShikataGaNai.pm
+++ encoders/ShikataGaNai.pm
@@ -11,7 +11,7 @@ my $bmb = 'Pex::Poly::BlockMaster::Block';
 my $bm = 'Pex::Poly:BlockMaster';
 
 my $advanced = {
-  'EncodeEnd' => [0, 'Encode last 4 bytes of decoder'],
+  'EncodeEnd' => [1, 'Encode last 4 bytes of decoder'],
 };
 
 my $info = {
