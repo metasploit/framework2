b85a27956130e6a244ccfa7c8fdecfdae3db6925
Spoon M <spoonm@gmail.com>
2004-07-05 09:34:27 +0000

    Raw udp foo.

---
+++ lib/Msf/Socket/RawUdpBase.pm
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+###############
+
+##
+#         Name: Socket.pm
+#       Author: spoonm <ninjatools [at] hush.com>
+#      Version: $Revision$
+#  Description: Socket wrapper around Pex::Socket.
+#      License:
+#
+#      This file is part of the Metasploit Exploit Framework
+#      and is subject to the same licenses and copyrights as
+#      the rest of this package.
+#
+##
+
+package Msf::Socket::RawUdpBase;
+use strict;
+use base 'Msf::Socket::SocketBase';
+
+1;
