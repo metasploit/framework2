0b2d1737c644615c359d58cb59c6cf3f1d1d8191
Spoon M <spoonm@gmail.com>
2004-07-20 06:47:55 +0000

    it was the tabs, I swear

--- lib/Pex/Text.pm
+++ lib/Pex/Text.pm
@@ -204,21 +204,22 @@ sub PatternCreate {
 }
 
 sub PatternOffset {
-       my ($pattern, $address) = @_;
-       my $endian = @_ ? shift() : 'V';
-       my @results;
-       my ($idx, $lst) = (0,0);
+     my $pattern = shift;
+     my $address = shift;
+     my $endian = @_ ? shift() : 'V';
+     my @results;
+     my ($idx, $lst) = (0,0);
 
-       $address = pack($endian, hex($address));
-       $idx = index($pattern, $address, $lst);
+     $address = pack($endian, hex($address));
+     $idx = index($pattern, $address, $lst);
 
-       while ($idx > 0)
-       {
-            push @results, $idx;
-            $lst = $idx + 1;
-            $idx = index($pattern, $address, $lst);
-       }
-       return @results;
+     while ($idx > 0)
+     {
+          push @results, $idx;
+          $lst = $idx + 1;
+          $idx = index($pattern, $address, $lst);
+     }
+     return @results;
 }
 
 1;
