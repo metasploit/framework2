b52667f2a152d47dbd1a69dd49dd2c7eb5f94611
HD Moore <hd_moore@rapid7.com>
2004-07-21 01:38:59 +0000

    Needs to check bit shifted key against badchars still ;/

--- encoders/Sparc.pm
+++ encoders/Sparc.pm
@@ -20,7 +20,7 @@ my $advanced =
 my $info = {
     'Name'    => 'Sparc DWord Xor Encoder',
     'Version' => '1.0',
-    'Authors' => [ 'optyx <optyx@uberhax0r.net>' ]
+    'Authors' => [ 'optyx <optyx@uberhax0r.net>' ],
     'Arch'    => [ 'sparc' ],
     'OS'      => [ ],
     'Description'  =>  "<optyx> fucking xor decoder 48 bytes, cache safe",
@@ -37,39 +37,37 @@ sub EncodePayload {
     my $payload  = shift;
     my $badchars = shift;
 
-    my $xor_key   = Pex::Encoder::XorKeyScanDword($payload, $badchars);
+    my $xor_key   = Pex::Encoder::XorDwordFeedback($payload, $badchars);
     my $xor_data  = Pex::Encoder::XorDword($xor_key, $payload);
 
     my $encoder = 
+        "\x20\xbf\xff\xff".   # /* bn,a  _start - 4 */
+        "\x20\xbf\xff\xff".   # /* bn,a  _start     */
+        "\x7f\xff\xff\xff".   # /* call  _start + 4 */
+        "\x2f\x10\x50\x50".   # /* sethi %hi(0x41414141),%l7 */
+        "\xae\x15\xe1\x41".   # /* or    %l7,%lo(0x41414141),%l7 */
+        "\x9e\x03\xe0\x24".   # /* add   %o7,0x24,%o7 */
+        "\xea\x03\xe0\x04".   # /* ld    [%o7 + 4],%l7 */
+        "\xaa\x9d\x40\x17".   # /* xorcc %l5,%l7,%l5 */
+        "\xea\x23\xe0\x04".   # /* st    %l5,[%o7 + 4] */
+        "\xae\x05\xc0\x15".   # /* add   %l7,%l5,%l7 */
+        "\x12\xbf\xff\xfc".   # /* bnz   dec_loop */
+        "\x9e\x03\xe0\x04";   # /* add   %o7,4,%o7 */
     
-    "\x20\xbf\xff\xff".   # /* bn,a  _start - 4 */
-    "\x20\xbf\xff\xff".   # /* bn,a  _start     */
-    "\x7f\xff\xff\xff".   # /* call  _start + 4 */
-
-	# 22 bits high
-    "\x2f\x10\x50\x50".   # /* sethi %hi(0x41414141),%l7 */
-
-	# 10 bits low
-    "\xae\x15\xe1\x41".   # /* or    %l7,%lo(0x41414141),%l7 */
+    # Extract
+    my $hiData = unpack('N', substr($encoder, 16, 4));
+    my $loData = unpack('N', substr($encoder, 20, 4));
+	
+    # Patch
+    $hiData = (($hiData >> 22) << 22) + ($xor_key >> 10);
+    $loData = (($loData >> 10) << 10) + (($xor_key << 22) >> 22);
     
-    "\x9e\x03\xe0\x24".   # /* add   %o7,0x24,%o7 */
-                          # /* dec_loop: */
-    "\xea\x03\xe0\x04".   # /* ld    [%o7 + 4],%l7 */
-    "\xaa\x9d\x40\x17".   # /* xorcc %l5,%l7,%l5 */
-    "\xea\x23\xe0\x04".   # /* st    %l5,[%o7 + 4] */
-    "\xae\x05\xc0\x15".   # /* add   %l7,%l5,%l7 */
-    "\x12\xbf\xff\xfc".   # /* bnz   dec_loop */
-    "\x9e\x03\xe0\x04";   # /* add   %o7,4,%o7 */
+    # Replace
+    substr($encoder, 16, 4, pack('N', $hiData));
+    substr($encoder, 20, 4, pack('N', $loData));
   
-   # TODO:
-   
-   #   split the key up into 22/10 format, mash with opcodes
-   #   determine whether we need a keyscan routine to find keys
-   #   whose 22/10 bit formats contain restricted chars 
-   #   append xor key to end of data
-   #   test, test, test, test, test...
-   #   buy optyx much liquor
- 
+    # XXX - We do not check to see if the split bitshifted key is a badchar!
+	return $encoder . $xor_data . pack('N', $xor_key); 
 }
 
 1;
