adf8493324628bac186674775a86440bb6d84434
HD Moore <hd_moore@rapid7.com>
2004-08-07 21:39:27 +0000

    Reorganized dev

---
+++ sdk/patternOffset.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use strict;
+use lib 'lib';
+use Pex::Text;
+
+die "0x01020304 length" if(!@ARGV);
+
+my $addr = shift;
+my $length = @_ ? shift : 200;
+
+print join(', ', Pex::Text::PatternOffset(Pex::Text::PatternCreate($length), $addr)) . "\n";
