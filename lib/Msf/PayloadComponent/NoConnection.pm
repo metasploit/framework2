4244feb4d8c3c6ab8c6426ba7da95a7d91466bfc
HD Moore <hd_moore@rapid7.com>
2004-08-13 18:08:33 +0000

    Growl.

--- lib/Msf/PayloadComponent/NoConnection.pm
+++ lib/Msf/PayloadComponent/NoConnection.pm
@@ -4,7 +4,16 @@ package Msf::PayloadComponent::NoConnection;
 use strict;
 use base 'Msf::PayloadComponent::ConnectionHandler';
 
-# What, empty file?
-# Yes, yes, I know.
+my $info = {
+  'Keys' => ['noconn'],
+};
+
+sub new {
+  my $class = shift;
+  my $self = $class->SUPER::new(@_);
+  $self->_Info($self->MergeHashRec($info, $self->_Info));
+  return($self);
+}
+
 
 1;
