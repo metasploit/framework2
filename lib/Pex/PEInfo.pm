651823f5e15c57942cb14451ae5617f9b17d1d36
HD Moore <hd_moore@rapid7.com>
2004-09-27 14:21:32 +0000

    Closer

--- lib/Pex/PEInfo.pm
+++ lib/Pex/PEInfo.pm
@@ -34,9 +34,19 @@ sub _Init {
     $self->{'OPT_IMG_HDR'} = { };
     $self->{'RVA'}         = { };
     $self->{'SECTIONS'}    = { };
+    $self->{'IMPORT'}      = { };
+    $self->{'EXPORT'}      = { };
+    
+    $self->Debug(1);
     
     $self->LoadImage($args);
     return $self;
+}
+
+sub Debug {
+    my $self = shift;
+    if (@_) { $self->{'Debug'} = shift() }
+    return $self->{'Debug'};
 }
 
 sub LastError {
@@ -108,7 +118,10 @@ sub Sections {
 
 sub ImageBase {
     my $self = shift;
-    $self->{'OPT_IMG_HDR'}->{'ImageBase'} = hex(shift()) if @_;
+    if (@_) {
+        $self->{'OPT_IMG_HDR'}->{'ImageBase'} = hex(shift());
+        $self->_LoadImport();
+    }
     return $self->{'OPT_IMG_HDR'}->{'ImageBase'};
 }
 
@@ -147,8 +160,7 @@ sub LoadImage {
     $IMAGE_HDR{'SizeOfOptionalHeader'}    = unpack('v', substr($data, $peo + 20));
     $IMAGE_HDR{'Characteristics'}         = unpack('v', substr($data, $peo + 22));
 
-    if ($IMAGE_HDR{'SizeOfOptionalHeader'} > 0)
-    {
+    if ($IMAGE_HDR{'SizeOfOptionalHeader'} > 0) {
         my $opthdr = substr($data, $peo + 24, $IMAGE_HDR{'SizeOfOptionalHeader'});
 
         $OPT_IMAGE_HDR{'Magic'}               = unpack('v', substr($opthdr, 0));
@@ -241,61 +253,188 @@ sub LoadImage {
         $SECTIONS{$sec_name}->[4] = $SECTIONS{$sec_name}->[1] - $SECTIONS{$sec_name}->[3];
     }   
     
-    foreach (keys(%IMAGE_HDR)) { printf("%s\t0x%.8x\n", $_ , $IMAGE_HDR{$_}); }
-    foreach (keys(%OPT_IMAGE_HDR)) { printf("%s\t0x%.8x\n", $_ , $OPT_IMAGE_HDR{$_}); }
-    foreach (keys(%RVA)) { printf("%s\t0x%.8x [0x%.8x]\n", $_ , $RVA{$_}->[0], $RVA{$_}->[1] ); }
-    foreach (keys(%SECTIONS)) 
-    { 
-        printf("%s\t0x%.8x\t0x%.8x\t0x%.8x\t0x%.8x\t0x%.8x\n",
-               $_ , $SECTIONS{$_}->[0], $SECTIONS{$_}->[1], $SECTIONS{$_}->[2], $SECTIONS{$_}->[3], $SECTIONS{$_}->[4]);
+    
+    if ($self->Debug) {
+        foreach (keys(%IMAGE_HDR)) { printf("%s\t0x%.8x\n", $_ , $IMAGE_HDR{$_}); }
+        foreach (keys(%OPT_IMAGE_HDR)) { printf("%s\t0x%.8x\n", $_ , $OPT_IMAGE_HDR{$_}); }
+        foreach (keys(%RVA)) { printf("%s\t0x%.8x [0x%.8x]\n", $_ , $RVA{$_}->[0], $RVA{$_}->[1] ); }
+        foreach (keys(%SECTIONS)) 
+        { 
+            printf("%s\t0x%.8x\t0x%.8x\t0x%.8x\t0x%.8x\t0x%.8x\n",
+                   $_ , $SECTIONS{$_}->[0], $SECTIONS{$_}->[1], $SECTIONS{$_}->[2], $SECTIONS{$_}->[3], $SECTIONS{$_}->[4]);
+        }
     }
     
     $self->{'IMG_HDR'}      = \%IMAGE_HDR;
     $self->{'OPT_IMG_HDR'}  = \%OPT_IMAGE_HDR;
     $self->{'SECTIONS'}     = \%SECTIONS;
     $self->{'RVA'}          = \%RVA;
+    
+    $self->_LoadImport();
+    $self->_LoadExport();
 
     return($self);    
 }
 
-sub OffsetToVirtual {
-    my ($self, $offset) = @_;
 
-    # Return zero if no ImageBase has been assigned yet
-    if (! $self->{'OPT_IMG_HDR'}->{'ImageBase'}) {
-        return(0);
-    }
+sub _LoadImport {
+    my $self = shift;
+    my $data = $self->{'RAW'};
+    my $import = $self->Rva('import');
+    my $itable = {};
+    
+    return if ! $import;
 
-    foreach (keys(%{ $self->{'SECTIONS'} }))  {
-        my @section = @{ $self->{'SECTIONS'}->{$_} };
-        if ( $offset >= $section[3] && $offset < ($section[2] + $section[3]) ) {
-            return($self->{'OPT_IMG_HDR'}->{'ImageBase'} + $offset + $section[4]);
-        }
-    }   
+    # Obtain the IMAGE_IMPORT_DESCRIPTOR array elements
+    my $idata = substr($data, $self->_RV2O($import->[0]), $import->[1]);
+   
+    # The number of import modules  
+    my $count = ($import->[1] / 20) - 1;
+
+    for my $idx (0 .. ($count - 1)) {
+        my @entry = unpack('VVVVV', substr($idata, $idx * 20, 20));
         
-    # Not in any given section, return the offset + ImageBase    
-    return($self->{'OPT_IMG_HDR'}->{'ImageBase'} + $offset);
+        if ($self->Debug) {
+            print "\n[ IMPORT $idx ]\n";
+            foreach (@entry) {
+                printf("\t0x%.8x\n", $_);
+            }
+        }
+        
+        # all null struct can signify end of array
+        last if ! $entry[0];
+        
+        my $oft_start_ref = $self->_RV2O($entry[0]);   # OrigFirstThunk
+        my $dll_name_ref  = $self->_RV2O($entry[3]);   # DLL name
+        my $rft_start_ref = $self->_RV2O($entry[4]);   # FirstThunk
+        my $dll_name      = unpack('Z*', substr($data, $dll_name_ref, 256));
+         
+        my ($eidx, $erva) = (0, 1);       
+        while ($erva != 0) {
+            $erva = unpack('V',  substr($data, $oft_start_ref + (4 * $eidx), 4));
+            next if ! $erva;
+            
+            my $entry_start_ref = $self->_RV2O($erva);
+            
+            my ($entry_name, $entry_hint_ord);
+            if ($erva & 0x80000000) {
+                my $ord = ($erva - 0x80000000);
+                $entry_name     = "#$ord";
+                $entry_hint_ord = $ord;
+            }
+            else {
+                # Catch some really broken DLL's here
+                if (! $entry_start_ref) {
+                    print STDERR "PEInfo: invalid RVA $erva\n";
+                    last;
+                }
+
+                $entry_name      = unpack('Z*', substr($data, $entry_start_ref+2, 256));
+                $entry_hint_ord  = unpack('v', substr($data, $entry_start_ref, 2));
+            }
+
+            my $entry_iat_add   = $self->_O2V($rft_start_ref + (4 * $eidx));
+            my $entry_iat_ref   = unpack('V', substr($data, $rft_start_ref + (4 * $eidx), 4));            
+            
+            if ($self->Debug) {
+                printf("[ %s ]\t%.3d 0x%.8x 0x%.8x %.4d [0x%.8x|0x%.8x] %s\n",
+                        lc($dll_name),
+                        $eidx,
+                        $oft_start_ref + (4 * $eidx),
+                        $erva, 
+                        $entry_hint_ord,
+                        $entry_iat_add,
+                        $entry_iat_ref,
+                        $entry_name,
+                        );
+            }        
+
+            $itable->{lc($dll_name)}->{$entry_name}->{'ord'} = $entry_hint_ord;
+            $itable->{lc($dll_name)}->{$entry_name}->{'iat'} = $entry_iat_add;
+            $itable->{lc($dll_name)}->{$entry_name}->{'iat_res'} = $entry_iat_ref;
+            $eidx++;
+        }    
+    }
+    $self->{'IMPORT'} = $itable;
 }
 
-sub VirtualToOffset {
-    my ($self, $virtual) = @_;
-    if (! $virtual) { return(0) }
+sub _LoadExport {
+    my $self = shift;
+    my $data = $self->{'RAW'};
+    my $export = $self->Rva('export');
+    my $etable = {};
+
+    return if ! $export;
+
+    # Obtain the IMAGE_EXPORT_DIRECTORY structure
+    my $edata  = substr($self->{'RAW'}, $self->_RV2O($export->[0]), $export->[1]);
+    my @fields = unpack('V10', $edata);
     
-    # Return zero if no ImageBase has been assigned yet
-    if (! $self->{'OPT_IMG_HDR'}->{'ImageBase'}) {
-        return(0);
+    # fields[0] = Characteristics
+    # fields[1] = TimeDateStamp
+    # fields[2] = Major/Minor Version
+    # fields[3] = Name RVA
+    # fields[4] = Base
+    # fields[5] = Number of Functions
+    # fields[6] = Number of Names
+    # fields[7] = Array of Functions
+    # fields[8] = Array of Names
+    # fields[9] = Array of Ordinals
+
+    if ($self->Debug) {
+        print "\n[ EXPORT ]\n";
+        foreach (@fields) {
+            printf("\t0x%.8x\n", $_);   
+        }
+        print "\n";
     }
     
-    $virtual -= $self->{'OPT_IMG_HDR'}->{'ImageBase'};
+    $etable->{'TimeDate'} = $fields[1];
+    $etable->{'name'} = lc
+    ( 
+        unpack('Z*', substr($data, 
+        $self->_RV2O($fields[3]), 256))
+    );
     
-    foreach (keys(%{ $self->{'SECTIONS'} })) {
-        my @section = @{ $self->{'SECTIONS'}->{$_} };
-        if ($virtual >= $section[1] && $virtual <= ($section[0] + $section[1])) {
-            return $virtual - $section[4];
+    my $func_ptr = $self->_RV2O($fields[7]);
+    my $name_ptr = $self->_RV2O($fields[8]);
+    my $ord_ptr  = $self->_RV2O($fields[9]);
+
+    # build the ordinal -> address map first
+    my @ord_table = ();
+    for (my $idx = 0; $idx < $fields[5]; $idx++) {
+        my $func_cur = unpack('V',  substr($data, $func_ptr + (4 * $idx), 4));
+        $ord_table[$idx] = $func_cur;
+    }
+
+    # scan the name -> ordinal map and match it up
+    for (my $idx = 0; $idx < $fields[5]; $idx++) {
+
+        # Pull the ordinal number for this name
+        my $ord_cur  = unpack('v',  substr($data, $ord_ptr  + (2 * $idx), 2));
+
+        # Match the ordinal to the function RVA
+        my $func_cur = $ord_table[$ord_cur];
+
+        # Make sure we didn't run out of names
+        next if $idx > ($fields[6] - 1);
+
+        # Read the function name from the names table                
+        my $name_cur = unpack('V',  substr($data, $name_ptr + (4 * $idx), 4));
+        my $name_str = unpack('Z*', substr($data, $self->_RV2O($name_cur), 512));
+
+        # Add the ordinal base value
+        $ord_cur += $fields[4];
+
+        $etable->{$etable->{'name'}}->{$name_str}->{'ord'} = $ord_cur;
+        $etable->{$etable->{'name'}}->{$name_str}->{'add'} = $func_cur + $self->ImageBase;
+        
+        if ($self->Debug) {    
+            printf("0x%.8x %.4d %s\n", $func_cur, $ord_cur, $name_str);
         }
     }
-   
-    return;
+
+    $self->{'EXPORT'} = $etable;
 }
 
 sub FindPEOffset {
@@ -303,6 +442,60 @@ sub FindPEOffset {
     my $peo = unpack('V', substr(${$data_ref}, 0x3c, 4));
     if (substr(${$data_ref}, 0, 2) ne 'MZ'  || substr(${$data_ref}, $peo, 2) ne 'PE') { return } 
     return($peo);
+}
+
+
+sub OffsetToRVA {
+    my ($self, $offset) = @_;
+    return 0 if ! defined($offset);
+    foreach (keys %{ $self->{'SECTIONS'} })  {
+        my @section = @{ $self->{'SECTIONS'}->{$_} };
+        if ( $offset >= $section[3] && $offset < ($section[2] + $section[3]) ) {
+            return $offset + $section[4];
+        }
+    }   
+}
+
+sub RVAToOffset {
+    my ($self, $virtual) = @_;
+    return 0 if ! defined($virtual);
+    foreach (keys %{ $self->{'SECTIONS'} }) {
+        my @section = @{ $self->{'SECTIONS'}->{$_} };
+        if ($virtual >= $section[1] && $virtual <= ($section[0] + $section[1])) {
+            return $virtual - $section[4];
+        }
+    }
+}
+
+sub OffsetToVirtual {
+    my ($self, $offset) = @_;
+    return $self->OffsetToRVA($offset) + $self->{'OPT_IMG_HDR'}->{'ImageBase'};
+}
+
+sub VirtualToOffset {
+    my ($self, $virtual) = @_;
+    $virtual -= $self->{'OPT_IMG_HDR'}->{'ImageBase'};
+    return $self->RVAToOffset($virtual);
+}
+
+sub _V2O {
+    my $self = shift;
+    return $self->VirtualToOffset(@_);
+}
+
+sub _RV2O {
+    my $self = shift;
+    return $self->RVAToOffset(@_);
+}
+
+sub _O2V {
+    my $self = shift;
+    return $self->OffsetToVirtual(@_);
+}
+
+sub _O2RV {
+    my $self = shift;
+    return $self->OffsetToRVA(@_);
 }
 
 1;
