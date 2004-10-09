9f7bc651281b56c961e024345da5373e8c34dee2
Spoon M <spoonm@gmail.com>
2004-10-09 21:16:57 +0000

    Removed the old expliciticly prefixed entries from the table

--- lib/Msf/Nop/OptyNop.pm
+++ lib/Msf/Nop/OptyNop.pm
@@ -54,77 +54,51 @@ my $prefix = $asize << 1;
 
 my $table = [
   [ "\xfe\xc0",           2, $reg1, [ $reg ] ], # /* incb %reg1 */
-#  [ "\x66\x40",           2, $reg1, [ $reg ] ], # /* incw %reg1 */
   [ "\x40",               1, $reg1, [ $reg ] ], # /* incl %reg1 */
   [ "\xfe\xc8",           2, $reg1, [ $reg ] ], # /* decb %reg1 */
-#  [ "\x66\x48",           2, $reg1, [ $reg ] ], # /* decw %reg1 */
   [ "\x48",               1, $reg1, [ $reg ] ], # /* decl %reg1 */
-#  [ "\x66\x50",           2, $reg1, [ $esp ] ], # /* pushw %reg1 */
-#  [ "\x66\x68",           4, $none, [ $esp ] ], # /* pushw $imm16 */
   [ "\x50",               1, $reg1, [ $esp ] ], # /* pushl %reg1 */
   [ "\x68",               5, $none, [ $esp ] ], # /* pushl $imm32 */
   [ "\x88\xc0",           2, $reg2, [ $reg ] ], # /* movb %reg1,%reg2 */
   [ "\xb0",               2, $reg1, [ $reg ] ], # /* movb $imm8,%reg1 */
-#  [ "\x66\x89\xc0",       3, $reg2, [ $reg ] ], # /* movw %reg1,%reg2 */
-#  [ "\x66\xb8",           4, $reg1, [ $reg ] ], # /* movw $imm16,%reg1 */
   [ "\x89\xc0",           2, $reg2, [ $reg ] ], # /* movl %reg1,%reg2 */
   [ "\xb8",               5, $reg1, [ $reg ] ], # /* movl $imm32,%reg1 */
   [ "\x30\xc0",           2, $reg2, [ $reg ] ], # /* xorb %reg1,%reg2 */
   [ "\x80\xf0",           3, $reg1, [ $reg ] ], # /* xorb $imm8,%reg1 */
-#  [ "\x66\x31\xc0",       3, $reg2, [ $reg ] ], # /* xorw %reg1,%reg2 */
-#  [ "\x66\x81\xf0",       5, $reg1, [ $reg ] ], # /* xorw $imm16,%reg1 */
   [ "\x31\xc0",           2, $reg2, [ $reg ] ], # /* xorl %reg1,%reg2 */
   [ "\x81\xf0",           6, $reg1, [ $reg ] ], # /* xorl $imm32,%reg1 */
   [ "\xf6\xe0",           2, $reg1, [ $eax ] ], # /* mulb %reg1 */
-#  [ "\x66\xf7\xe0",       3, $reg1, [ $eax, $edx ] ], # /* mulw %reg1 */
   [ "\xf7\xe0",           2, $reg1, [ $eax, $edx ] ], # /* mull %reg1 */
   [ "\x20\xc0",           2, $reg2, [ $reg ] ], # /* andb %reg1,%reg2 */
   [ "\x80\xe0",           3, $reg1, [ $reg ] ], # /* andb $imm8,%reg1 */
-#  [ "\x66\x21\xc0",       3, $reg2, [ $reg ] ], # /* andw %reg1,%reg2 */
-#  [ "\x66\x81\xe0",       5, $reg1, [ $reg ] ], # /* andw $imm16,%reg1 */
   [ "\x21\xc0",           2, $reg2, [ $reg ] ], # /* andl %reg1,%reg2 */
   [ "\x81\xe0",           6, $reg1, [ $reg ] ], # /* andl $imm32,%reg1 */
   [ "\x08\xc0",           2, $reg2, [ $reg ] ], # /* orb %reg1,%reg2 */
   [ "\x80\xc8",           3, $reg1, [ $reg ] ], # /* orb $imm8,%reg1 */
   [ "\x0c",               2, $none, [ $eax ] ], # /* orb $imm8,%al */
-#  [ "\x66\x09\xc0",       3, $reg2, [ $reg ] ], # /* orw %reg1,%reg2 */
-#  [ "\x66\x81\xc8",       5, $reg1, [ $reg ] ], # /* orw $imm16,%reg1 */
-#  [ "\x66\x0d",           4, $none, [ $reg ] ], # /* orw $imm16,%ax */
   [ "\x09\xc0",           2, $reg2, [ $reg ] ], # /* orl %reg1,%reg2 */
   [ "\x81\xc8",           6, $reg1, [ $reg ] ], # /* orl $imm32,%reg1 */
   [ "\x0d",               5, $none, [ $eax ] ], # /* orl $imm32,%eax */
   [ "\x04",               2, $none, [ $eax ] ], # /* addb $imm8,%al */
   [ "\x80\xc0",           3, $reg1, [ $reg ] ], # /* addb $imm8,%reg1 */
-#  [ "\x66\x01\xc0",       3, $reg2, [ $reg ] ], # /* addw %reg1,%reg2 */
-#  [ "\x66\x81\xc0",       5, $reg1, [ $reg ] ], # /* addw $imm16,%reg1 */
-#  [ "\x66\x05",           4, $none, [ $eax ] ], # /* addw $imm16,%ax */
   [ "\x01\xc0",           2, $reg2, [ $reg ] ], # /* addl %reg1,%reg2 */
   [ "\x81\xc0",           6, $reg2, [ $reg ] ], # /* addl $imm32,%reg1 */
   [ "\x05",               5, $none, [ $eax ] ], # /* addl $imm32,%eax */
   [ "\x28\xc0",           2, $reg2, [ $reg ] ], # /* subb %reg1,%reg2 */
   [ "\x80\xe8",           3, $reg1, [ $reg ] ], # /* subb $imm8,%reg1 */
-#  [ "\x66\x29\xc0",       3, $reg2, [ $reg ] ], # /* subw %reg1,%reg2 */
-#  [ "\x66\x81\xe8",       5, $reg1, [ $reg ] ], # /* subw $imm16,%reg1 */
   [ "\x29\xc0",           2, $reg2, [ $reg ] ], # /* subl %reg1,%reg2 */
   [ "\x81\xe8",           6, $reg1, [ $reg ] ], # /* subl $imm32,%reg1 */
   [ "\xf6\xd0",           2, $reg1, [ $reg ] ], # /* notb %reg1 */
-#  [ "\x66\xf7\xd0",       3, $reg1, [ $reg ] ], # /* notw %reg1 */
   [ "\xf7\xd0",           2, $reg1, [ $reg ] ], # /* notl %reg1 */
   [ "\x84\xc0",           2, $reg2, [ ] ], # /* testb %reg1,%reg2 */
   [ "\xf6\xc0",           3, $reg1, [ ] ], # /* testb $imm8,%reg1 */
   [ "\xa8",               2, $none, [ ] ], # /* testb $imm8,%al */
-#  [ "\x66\x85\xc0",       3, $reg2, [ ] ], # /* testw %reg1,%reg2 */
-#  [ "\x66\xf7\xc0",       5, $reg1, [ ] ], # /* testw $imm16,%reg1 */
-#  [ "\x66\xa9",           4, $none, [ ] ], # /* testw $imm16,%ax */
   [ "\x85\xc0",           2, $reg2, [ ] ], # /* testl %reg1,%reg2 */
   [ "\xf7\xc0",           6, $reg1, [ ] ], # /* testl $imm32,%reg1 */
   [ "\xa9",               5, $none, [ ] ], # /* testl $imm32,%eax */
   [ "\x38\xc0",           2, $reg2, [ ] ], # /* cmpb %reg1,%reg2 */
   [ "\x3c",               2, $none, [ ] ], # /* cmpb $imm8,%al */
   [ "\x80\xf8",           3, $reg1, [ ] ], # /* cmpb $imm8,%reg1 */
-#  [ "\x66\x39\xc0",       3, $reg2, [ ] ], # /* cmpw %reg1,%reg2 */
-#  [ "\x66\x3d",           4, $none, [ ] ], # /* cmpw $imm16,%ax */
-#  [ "\x66\x81\xf8",       5, $reg1, [ ] ], # /* cmpw $imm16,%reg1 */
   [ "\x39\xc0",           2, $reg2, [ ] ], # /* cmpl %reg1,%reg2 */
   [ "\x3d",               5, $none, [ ] ], # /* cmpl $imm32,%eax */
   [ "\x81\xf8",           6, $reg1, [ ] ], # /* cmpl $imm32,%reg1 */
@@ -138,15 +112,11 @@ my $table = [
   [ "\x06",               1, $none, [ $esp ] ], # /* push %es */
   [ "\x0e",               1, $none, [ $esp ] ], # /* push %cs */
   [ "\x14",               2, $none, [ $eax ] ], # /* adc $imm8,%al */
-#  [ "\x66\x15",           4, $none, [ $eax ] ], # /* adc $imm16,%ax */
   [ "\x15",               5, $none, [ $eax ] ], # /* adc $imm32,%eax */
   [ "\x16",               1, $none, [ $esp ] ], # /* push %ss */
   [ "\x1c",               2, $none, [ $eax ] ], # /* sbbb $imm8,%al */
-#  [ "\x66\x1d",           4, $none, [ $eax ] ], # /* sbbw $imm16,%ax */
   [ "\x1d",               5, $none, [ $eax ] ], # /* sbbl $imm32,%eax */
   [ "\x80\xd8",           3, $reg1, [ $reg ] ], # /* sbbb $imm8,%reg1 */
-#  [ "\x66\x83\xd8",       4, $reg1, [ $reg ] ], # /* sbbw $imm8,%reg1 */
-#  [ "\x66\x81\xd8",       5, $reg1, [ $reg ] ], # /* sbbw $imm16,%reg1 */
   [ "\x83\xd8",           3, $reg1, [ $reg ] ], # /* sbbl $imm8,%reg1 */
   [ "\x81\xd8",           6, $reg1, [ $reg ] ], # /* sbbl $imm32,%reg1 */
   [ "\x1e",               1, $none, [ $esp ] ], # /* push %ds */
