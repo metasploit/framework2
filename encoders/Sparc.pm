7e1b48d5d90d1825bbebe69bb4041c9711907a92
no_author <no_author@no_author>
2004-11-05 04:28:00 +0000

    1. Use Pex::SPARC::set() rather then bit shifts. 2. Add a $Revision: 1.0 $ tag.

--- encoders/Sparc.pm
+++ encoders/Sparc.pm
@@ -1,5 +1,5 @@
 
-##
+#
 # This file is part of the Metasploit Framework and may be redistributed
 # according to the licenses defined in the Authors field below. In the
 # case of an unknown or missing license, this file defaults to the same
@@ -11,6 +11,7 @@ package Msf::Encoder::Sparc;
 use strict;
 use base 'Msf::Encoder';
 use Pex::Encoding::XorDwordFeedbackN;
+use Pex::SPARC;
 
 my $advanced = 
 {
@@ -19,7 +20,7 @@ my $advanced =
 
 my $info = {
     'Name'    => 'Sparc DWord Xor Encoder',
-    'Version' => '1.0',
+    'Version' => '$Revision$',
     'Authors' => [ 'optyx <optyx@uberhax0r.net>' ],
     'Arch'    => [ 'sparc' ],
     'OS'      => [ ],
@@ -59,12 +60,14 @@ sub EncodePayload {
     $payload .= pack('N', 0);
     my $xor_data = Pex::Encoding::XorDwordFeedbackN->Encode($xor_key, $payload);
 
+    # Flip the key endian-ness
+    $xor_key = unpack('V', pack('N', $xor_key));
+
     my $encoder = 
         "\x20\xbf\xff\xff".   # /* bn,a  _start - 4 */
         "\x20\xbf\xff\xff".   # /* bn,a  _start     */
         "\x7f\xff\xff\xff".   # /* call  _start + 4 */
-        "\x2f\x10\x50\x50".   # /* sethi %hi(0x41414141),%l7 */
-        "\xae\x15\xe1\x41".   # /* or    %l7,%lo(0x41414141),%l7 */
+	Pex::SPARC::set($xor_key, "l7").
         "\xea\x03\xe0\x28".   # /* ld    [%o7 + 0x28],%l7 */
         "\xaa\x9d\x40\x17".   # /* xorcc %l5,%l7,%l5 */
         "\xea\x23\xe0\x28".   # /* st    %l5,[%o7 + 0x28] */
@@ -73,22 +76,6 @@ sub EncodePayload {
         "\x12\xbf\xff\xfb".   # /* bnz   dec_loop */
         "\x9e\x03\xe0\x04";   # /* add   %o7,4,%o7 */
     
-    
-    # Flip the key endian-ness
-    $xor_key = unpack('V', pack('N', $xor_key));
- 
-    # Extract
-    my $hiData = unpack('N', substr($encoder, 12, 4));
-    my $loData = unpack('N', substr($encoder, 16, 4));
-
-    # Patch
-    $hiData = (($hiData >> 22) << 22) + ($xor_key >> 10);
-    $loData = (($loData >> 10) << 10) + (($xor_key << 22) >> 22);
-    
-    # Replace
-    substr($encoder, 12, 4, pack('N', $hiData));
-    substr($encoder, 16, 4, pack('N', $loData));
-  
     # XXX - We do not check to see if the split bitshifted key is a badchar!
     return $encoder . $xor_data;
 }
