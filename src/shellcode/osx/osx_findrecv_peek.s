316549236d8815d1822d99b10043953d96e81704
HD Moore <hd_moore@rapid7.com>
2004-12-10 10:07:28 +0000

    Added headers back in

--- src/shellcode/osx/osx_findrecv_peek.s
+++ src/shellcode/osx/osx_findrecv_peek.s
@@ -1,3 +1,27 @@
+;;
+;
+;        Name: osx_findrecv_peek
+;   Qualities: Null-Free
+;   Platforms: MacOS X / PPC
+;     Authors: H D Moore <hdm [at] metasploit.com>
+;     Version: $Revision$
+;     License:
+;
+;        This file is part of the Metasploit Exploit Framework
+;        and is subject to the same licenses and copyrights as
+;        the rest of this package.
+;
+; Description:
+;
+;        This payload will recv() downward until the read
+;        data contains the search tag (0xXXXX1337). Once the
+;        tag is located, it will jump into the payload. The
+;        recv() call is passed the MSG_PEEK flag, the stage
+;        will need to flush the recv() queue before doing
+;        something like dup2'ing a shell.
+;
+;;
+
 .globl _main
 .text
 _main:
