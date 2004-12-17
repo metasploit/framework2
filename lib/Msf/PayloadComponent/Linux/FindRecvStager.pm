c0fa36bd06940632ccf748a8b8e6b2adeef31952
Matt Miller <mmiller@hick.org>
2004-12-17 08:09:15 +0000

    fixed up linux find stager

--- lib/Msf/PayloadComponent/Linux/FindRecvStager.pm
+++ lib/Msf/PayloadComponent/Linux/FindRecvStager.pm
@@ -23,13 +23,31 @@ use base 'Msf::PayloadComponent::Linux::Payload';
 use vars qw{@ISA};
 
 #
-# Dynamically inherit Payload from FindRecvConnection
+# Dynamically inherit Payload from FindConnection
 #
 sub _Load
 {
-	Msf::PayloadComponent::Linux::Payload->_Import('Msf::PayloadComponent::FindRecvConnection');
+	Msf::PayloadComponent::Linux::Payload->_Import('Msf::PayloadComponent::FindConnection');
 
 	__PACKAGE__->SUPER::_Load();
+}
+
+#
+# Handle the child socket that's passed to us by dishing out the tag
+#
+sub ChildHandler
+{
+	my $self = shift;
+	my $sock = shift;
+	my $stage = $self->BuildPayload($self->StagePayload);
+	my $tag   = substr($self->GetLocal('FINDTAG') . ("\x01" x 4), 0, 4);
+	my $data  = $tag . $stage;
+
+	eval { $sock->send($data); };
+
+
+	# XXX: We should only do this if the second stage is a shell payload...
+	return $self->SUPER::ChildHandler($sock);
 }
 
 1;
